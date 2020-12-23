c2eeb2ca05023aa767d1651b1dfa31738c406901
Byeu <35936063+Byeu@users.noreply.github.com>
2020-12-23 14:55:22 -0300

    _app e _doc config

---
+++ pages/addColab.js
@@ -0,0 +1,3 @@
+export default function Foi() {
+    return <div>FOI!</div>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
 function Home () {
-    return <div>Home</div>
+    return (
+        <div>
+            <p>O que é?</p>
+            <Avançar />
+        </div>
+    
+    )
+}
+
+function dirAddColab() {
+    return 
+}
+
+function Avançar() {
+    return (
+        <div>
+            <a href="../addColab">
+            <button>Avançar</button>
+            </a>
+        </div>
+    )
 }
 
 export default Home
